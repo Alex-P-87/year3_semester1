bfb158d58319af0618f0340799442a5dd5d725de
Alex <alexpor87&gmail.com>
2025-02-10 12:03:41 +0200

    2 new files

---
+++ countOccurrences.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function countCharacterOccurrences() {
+    local char=$1
+    local str="hello this is a checking function, here we check how many letters are found like a parameter"
+    local count=0
+    local i
+
+    for (( i=0; i<${#str}; i++ )); do
+        if [[ "${str:$i:1}" == "$char" ]]; then
+            ((count++))
+        fi
+    done
+
+    echo $count
+}
